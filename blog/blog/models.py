8bedb88cb7b49e136aa7c4c9a9dec91d5c85bfb3
Sebastian- <hmurgu@interchange.ubc.ca>
2018-09-05 13:22:51 -0700

    Created a db model for posts

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User', # reference to the built in Django user model
+        on_delete=models.CASCADE, # this option specifies that when an author model is deleted,
+                                  # all of their associated posts are also deleted
+    )
+    body = models.TextField()
+
+
+    def __str__(self):
+        return self.title
